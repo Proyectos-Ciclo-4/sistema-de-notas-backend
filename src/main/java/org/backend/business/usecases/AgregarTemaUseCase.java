307006d983b236210b201b96ac9b77b00ad06624
d-rocham <d_rocham@outlook.com>
2022-09-23 17:07:31 -0500

    Flujo de información para CrearTareaUseCase revisado

--- src/main/java/org/backend/business/usecases/AgregarTemaUseCase.java
+++ src/main/java/org/backend/business/usecases/AgregarTemaUseCase.java
@@ -1,11 +1,8 @@
 package org.backend.business.usecases;
 
-import com.mongodb.MongoNodeIsRecoveringException;
 import org.backend.application.repository.MongoEventRepository;
 import org.backend.application.repository.MongoViewRepository;
-import org.backend.business.models.vistasmaterializadas.VistaTarea;
 import org.backend.business.models.vistasmaterializadas.generics.TemaGeneric;
-import org.backend.domain.commands.CrearTarea;
 import org.backend.domain.commands.CrearTema;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;

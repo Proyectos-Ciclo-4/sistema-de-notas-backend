d0d94e3cd035a2e3d3aa719c68ed350984abcb17
d-rocham <d_rocham@outlook.com>
2022-09-24 12:29:40 -0500

    endpoints de creacion restantes implementados

--- src/main/java/org/backend/application/handlers/CommandHandle.java
+++ src/main/java/org/backend/application/handlers/CommandHandle.java
@@ -3,10 +3,9 @@ package org.backend.application.handlers;
 import co.com.sofka.domain.generic.DomainEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.backend.business.models.vistasmaterializadas.VistaEstudiante;
-import org.backend.business.usecases.CrearEstudianteUseCase;
-import org.backend.business.usecases.CrearProfesorUseCase;
-import org.backend.domain.commands.CrearEstudiante;
-import org.backend.domain.commands.CrearProfesor;
+import org.backend.business.models.vistasmaterializadas.generics.TemaGeneric;
+import org.backend.business.usecases.*;
+import org.backend.domain.commands.*;
 import org.backend.domain.events.EstudianteCreado;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,7 +39,7 @@ public class CommandHandle {
     }
 
     @Bean
-    public  RouterFunction<ServerResponse> crearProfesor(CrearProfesorUseCase crearProfesorUseCase) {
+    public RouterFunction<ServerResponse> crearProfesor(CrearProfesorUseCase crearProfesorUseCase) {
         return route(
                 POST("/crearProfesor")
                         .and(accept(MediaType.APPLICATION_JSON)),
@@ -54,6 +53,71 @@ public class CommandHandle {
                         })
         );
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> crearCurso(CrearCursoUseCase crearCursoUseCase) {
+        return route(
+                POST("/crearCurso")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                request -> crearCursoUseCase.apply(request.bodyToMono(CrearCurso.class))
+                        .flatMap(vistaCurso -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(vistaCurso))
+                        .onErrorResume(throwable -> {
+                            log.error(throwable.getMessage());
+                            return ServerResponse.badRequest().build();
+                        })
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> crearTema(AgregarTemaUseCase agregarTemaUseCase) {
+        return route(
+                POST("/crearTema")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                request -> agregarTemaUseCase.apply(request.bodyToMono(CrearTema.class))
+                        .flatMap(temaGeneric -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(temaGeneric))
+                        .onErrorResume(throwable -> {
+                            log.error(throwable.getMessage());
+                            return ServerResponse.badRequest().build();
+                        })
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> crearTarea(CrearTareaUseCase crearTareaUseCase) {
+        return route(
+                POST("/crearTarea")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                request -> crearTareaUseCase.apply(request.bodyToMono(CrearTarea.class))
+                        .flatMap(vistaTarea -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(vistaTarea))
+                        .onErrorResume(throwable -> {
+                            log.error(throwable.getMessage());
+                            return ServerResponse.badRequest().build();
+                        })
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inscribirEstudianteEnCurso(InscribirEstudianteACursoUseCase inscribirEstudianteEnCurso) {
+        return route(
+                POST("/inscribirEstudiante")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                request -> inscribirEstudianteEnCurso.apply(request.bodyToMono(CrearInscripcion.class))
+                        .flatMap(vistaEstudiante -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(vistaEstudiante))
+                        .onErrorResume(throwable -> {
+                            log.error(throwable.getMessage());
+                            return ServerResponse.badRequest().build();
+                        })
+        );
+    }
+
 
 
 }

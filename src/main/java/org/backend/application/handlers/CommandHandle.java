f9452aedb661845894f6e1df25b25930f07026a1
quintana29 <jhonatanquintana29@gmail.com>
2022-09-23 19:21:37 -0500

    Agregando endPoint de creación

--- src/main/java/org/backend/application/handlers/CommandHandle.java
+++ src/main/java/org/backend/application/handlers/CommandHandle.java
@@ -1,9 +1,40 @@
 package org.backend.application.handlers;
 
+import co.com.sofka.domain.generic.DomainEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.backend.business.models.vistasmaterializadas.VistaEstudiante;
+import org.backend.business.usecases.CrearEstudianteUseCase;
+import org.backend.domain.commands.CrearEstudiante;
+import org.backend.domain.events.EstudianteCreado;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Slf4j
 @Configuration
 public class CommandHandle {
+
+    @Bean
+    public RouterFunction<ServerResponse> crearEstudiante(CrearEstudianteUseCase crearEstudianteUseCase) {
+        return route(
+                POST("/crearEstudiante")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse
+                        .ok().contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(
+                                crearEstudianteUseCase.apply(
+                                        request.bodyToMono(CrearEstudiante.class)), VistaEstudiante.class
+
+                        ))
+        );
+
+
+    }
 }
--- src/main/java/org/backend/business/usecases/CrearEstudianteUseCase.java
+++ src/main/java/org/backend/business/usecases/CrearEstudianteUseCase.java
@@ -4,9 +4,9 @@ import org.backend.application.repository.MongoEventRepository;
 import org.backend.application.repository.MongoViewRepository;
 import org.backend.business.models.vistasmaterializadas.VistaEstudiante;
 import org.backend.domain.commands.CrearEstudiante;
-import org.backend.domain.commands.CrearProfesor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
+@Service
 public class CrearEstudianteUseCase {
     private final MongoViewRepository mongoViewRepository;
     private final MongoEventRepository mongoEventRepository;
@@ -19,7 +19,8 @@ public class CrearEstudianteUseCase {
     public Mono<VistaEstudiante> apply(Mono<CrearEstudiante> crearEstudianteMono){
         return crearEstudianteMono.flatMap(command -> {
                     VistaEstudiante estudiante = new VistaEstudiante(
-                            command.getEstudianteID()
+                            command.getEstudianteID(),
+                            command.getNombre()
 
                     );
                     return mongoViewRepository.crearEstudiante(estudiante);

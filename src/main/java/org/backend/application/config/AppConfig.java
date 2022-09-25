91e10f6a6af545e308d232a851f8056619956d2c
d-rocham <d_rocham@outlook.com>
2022-09-25 08:56:22 -0500

    Config boilerplate

---
+++ src/main/java/org/backend/application/config/AppConfig.java
@@ -0,0 +1,25 @@
+package org.backend.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public WebFluxConfigurer corsConfigure() {
+        return new WebFluxConfigurerComposite() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+
+    }
+}

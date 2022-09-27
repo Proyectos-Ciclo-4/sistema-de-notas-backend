016fc8947628c2a41c21800255176f2e9fb4c2f5
quintana29 <jhonatanquintana29@gmail.com>
2022-09-27 16:40:15 -0500

    Creación test crearCursoTest

--- src/test/java/org/backend/business/usecases/CrearEstudianteUseCaseTest.java
+++ src/test/java/org/backend/business/usecases/CrearEstudianteUseCaseTest.java
@@ -4,9 +4,7 @@ package org.backend.business.usecases;
 import org.backend.application.repository.MongoEventRepository;
 import org.backend.application.repository.MongoViewRepository;
 import org.backend.business.models.vistasmaterializadas.VistaEstudiante;
-import org.backend.domain.commands.CrearEstudiante;
 import org.backend.domain.events.EstudianteCreado;
-import org.backend.domain.identifiers.EstudianteID;
 import org.backend.domain.valueobjects.Nombre;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,7 +13,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
